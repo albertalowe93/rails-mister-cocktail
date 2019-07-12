b32cd2a27d493af1a3e16c0f1b05515f907119cd
Alberta Lowe <albertalowe93@gmail.com>
2019-07-12 11:05:52 -0300

    Updated Mister Cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  delete 'cocktails/:id', to: 'cocktails#destroy', as: 'destroy'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
